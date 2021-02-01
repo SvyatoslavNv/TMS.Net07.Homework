ea38a20aa444193b338ee181680c2996fe950256
SvyatoslavNv <nenahov.sa@tut.by>
2021-02-01 12:56:36 +0300

    task 2 middle

--- TMS.Net07.Homework.DaysOfWeek/TMS.Net07.Homework.DaysOfWeek/Program.cs
+++ TMS.Net07.Homework.DaysOfWeek/TMS.Net07.Homework.DaysOfWeek/Program.cs
@@ -8,73 +8,22 @@ namespace TMS.Net07.Homework.DaysOfWeek
 {
     class Program
     {
-        enum daysofweek
-        {
-            понедельник,
-            вторник,
-            среда,
-            четверг,
-            пятница,
-            суббота,
-            воскресенье
-        }
         static void Main(string[] args)
         {
-            daysofweek day;
-            string input = "enter";
+            DateTime data;
+            var input = "enter";
             while (input != "exit")
             {
-                Console.WriteLine("enter the day of the week (for exit enter \"exit\")");
+                Console.WriteLine("enter the date \"dd.mm.yyyy\" (for exit enter \"exit\")");
                 input = Console.ReadLine();
-                switch (input)
+                bool result = DateTime.TryParse(input, out data);
+                if (result == true)
                 {
-                    case "monday":
-                    {
-                        day = (daysofweek)0;
-                        Console.WriteLine(day);
-                        break;
-                    }
-                    case "tuesday":
-                    {
-                        day = (daysofweek)1;
-                        Console.WriteLine(day);
-                        break;
-                    }
-                    case "wednes­day":
-                    {
-                        day = (daysofweek)2;
-                        Console.WriteLine(day);
-                        break;
-                    }
-                    case "thursday":
-                    {
-                        day = (daysofweek)3;
-                        Console.WriteLine(day);
-                        break;
-                    }
-                    case "friday":
-                    {
-                        day = (daysofweek)4;
-                        Console.WriteLine(day);
-                        break;
-                    }
-                    case "saturday":
-                    {
-                        day = (daysofweek)5;
-                        Console.WriteLine(day);
-                        break;
-                    }
-                    case "sunday":
-                    {
-                        day = (daysofweek)6;
-                        Console.WriteLine(day);
-                        break;
-                    }
-                    default:
-                    {
-                        Console.WriteLine("incorrect data entry");
-                        break;
-                    }
+                    Console.WriteLine(data.DayOfWeek);
+                }
+                else
+                {
+                    Console.WriteLine($"\"{input}\" incorrect data entry");
                 }
             }
         }
